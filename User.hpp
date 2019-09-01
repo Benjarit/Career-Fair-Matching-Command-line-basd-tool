d73f5611ab5f8e6cf80e47f27bc46e267bf1ffc3
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-09-01 16:48:12 -0500

    Update User.hpp

--- User.hpp
+++ User.hpp
@@ -7,7 +7,7 @@
 class User{
     public:
         User();
-        User(string t_usernamee, string t_password): m_username(t_usernamee), m_password(t_password){};
+        User(string t_usernamee, string t_password): username(t_usernamee), password(t_password){};
         virtual ~User();
         virtual void printMenu() = 0; //a pure virtual function, makes it an ABC
         void changePassword();
@@ -20,6 +20,6 @@ class User{
         void setUsername(string newUsername);
         void setPassword(string newPassword);
     protected:
-        string m_username;
-        string m_password;
+        string username;
+        string password;
 };
