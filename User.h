16b0414608651f04121f6b7ed7b45be99af7a091
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-09-01 16:04:02 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,19 +1,25 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+#include <string.h>
+#include <stdlib.h>
+
 class User{
     public:
         User();
-        User(string newUsername, string newPassword){username = newUsername; password = newPassword;};
+        User(string t_usernamee, string t_password): m_username(t_usernamee), m_password(t_password){};
         virtual ~User();
         virtual void printMenu() = 0; //a pure virtual function, makes it an ABC
         void changePassword();
   
         // *getters
-        string getUsername(){return username;};
-        string getPassword(){return username;};
+        string getUsername();
+        string getPassword();
         
         // *setters
-        void setUsername(string newUsername){username = newUsername;};
-        void setPassword(string newPassword){password = newPassword;};
+        void setUsername(string newUsername);
+        void setPassword(string newPassword);
     protected:
-        string username;
-        string password;
+        string m_username;
+        string m_password;
 };
