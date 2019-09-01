daeec1b8b059dbb8eb9fb5a2d5b6b3b1df638270
Benjarit Hotrabhavananda <bhmcf@mail.missouri.edu>
2019-09-01 15:28:36 -0500

    Update User.h

--- User.h
+++ User.h
@@ -1,1 +1,19 @@
-
+class User{
+    public:
+        User();
+        User(string newUsername, string newPassword){username = newUsername; password = newPassword;};
+        virtual ~User();
+        virtual void printMenu() = 0; //a pure virtual function, makes it an ABC
+        void changePassword();
+  
+        // *getters
+        string getUsername(){return username;};
+        string getPassword(){return username;};
+        
+        // *setters
+        void setUsername(string newUsername){username = newUsername;};
+        void setPassword(string newPassword){password = newPassword;};
+    protected:
+        string username;
+        string password;
+};
